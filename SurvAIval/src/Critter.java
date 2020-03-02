3e441c00dcf156dea28619e507c41d049f0fdde1
Kaoe <Kaoelol@gmail.com>
2020-03-02 16:28:57 -0600

    Add files via upload

---
+++ SurvAIval/src/Critter.java
@@ -0,0 +1,15 @@
+
+public class Critter {
+	// Starting values for initial simulation(base stats)
+	final double SPEED = 1.0;
+	final int ANGLE = (int) (360 * Math.random());
+	final int SIZE = 1;
+	
+	// Base critters start with no mutations
+	boolean visionStick = false;
+	boolean cautious = false;
+	boolean tall = false;
+	boolean canSwim = false;
+	// TODO: Come up with more mutations
+	
+}
