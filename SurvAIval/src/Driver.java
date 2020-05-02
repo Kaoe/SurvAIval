be8b8b82f38351e63f67988600569da4148daed9
elliotwoolley <elliot@attributesoftware.com>
2020-05-01 22:39:25 -0500

    This is the driver I currently have

---
+++ SurvAIval/src/Driver.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Driver {
+    public static void main(String[] args) {
+
+
+        Critter critter = new  Critter(0,1,new int[] {0,0});
+
+        critter.doRandom(10);
+        System.out.println(critter.toString());
+
+        TileMap tileMap = new TileMap(3,4);
+
+        System.out.println(tileMap.toString());
+
+
+        List open = new ArrayList();
+        List closed = new ArrayList();
+        open.add(tileMap.getTile(2));
+
+
+
+
+
+
+    }
+}
