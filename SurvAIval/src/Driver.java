d25a76db737c4db37154edce6710b9a13971423f
elliotwoolley <elliot@attributesoftware.com>
2020-05-07 16:16:42 -0500

    Changed coords from array to simply int x and int y. Also added some check methods and improved the TileMap search method.

--- SurvAIval/src/Driver.java
+++ SurvAIval/src/Driver.java
@@ -5,19 +5,35 @@ public class Driver {
     public static void main(String[] args) {
 
 
-        Critter critter = new  Critter(0,1,0,0);
-
-        critter.doRandom(10);
-        System.out.println(critter.toString());
-
-        TileMap tileMap = new TileMap(3,4);
-
-        System.out.println(tileMap.toString());
 
 
-        List open = new ArrayList();
-        List closed = new ArrayList();
-        open.add(tileMap.getTile(2));
+        TileMap tileMap = new TileMap(10,15, 0.5);
+        Tile tile = null;
+
+        tile = tileMap.search(7,12);
+
+        System.out.println(tile.toString());
+
+        tile.isPassable();
+
+        System.out.println(tileMap.isValid(9,4));
+        System.out.println(tileMap.isPassable(9,4));
+
+
+
+//        Critter critter = new  Critter(0,1,0,0);
+//
+//        critter.doRandom(10);
+//        System.out.println(critter.toString());
+//
+//        TileMap tileMap = new TileMap(3,4);
+//
+//        System.out.println(tileMap.toString());
+//
+//
+//        List open = new ArrayList();
+//        List closed = new ArrayList();
+//        open.add(tileMap.getTile(2));
 
 
 
